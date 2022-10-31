d048841a206a2ea979d8126ae9ecbfacd301019f
Jared Cosulich <jared.cosulich@gmail.com>
2022-10-31 09:47:12 -0400

    new contract

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-  contractAddress: "0xb52e9f3aca37e4576d2b87d6c8d13aa99789a2cb",
+  contractAddress: "0xbd93c6189c12d477f09c62c1be663db0a41ae4e7",
   pieces: {
     '11': `
       <svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" viewBox="0 0 2048 2048" id="svg2" version="1.1" inkscape:version="0.48.2 r9819" width="100%" height="100%" sodipodi:docname="wp.svg">
