6db2d1f50795ec52abc5c0a0c710f401ce7ffb10
Jared Cosulich <jared.cosulich@gmail.com>
2022-11-22 15:29:22 -0500

    new contract

--- build/index.js
+++ build/index.js
@@ -68,7 +68,7 @@ module.exports = {
 }
 },{"./constants":2,"./utils":6}],2:[function(require,module,exports){
 module.exports = {
-  contractAddress: "0x961e63d62c2339b45fa2ecd214c5a1a15970e15c",
+  contractAddress: "0x96f9aeb62cebd369ca4b76f34d04c56bcaf02b06",
   pieces: {
     '11': `
       <svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" viewBox="0 0 2048 2048" id="svg2" version="1.1" inkscape:version="0.48.2 r9819" width="100%" height="100%" sodipodi:docname="wp.svg">
